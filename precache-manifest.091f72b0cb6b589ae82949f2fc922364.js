be72c1cc2d0dc390c087c56eaa9154fac8f341c7
ahmedm512 <1600155@eng.asu.edu.eg>
2021-06-10 02:24:25 +0200

    Updates

---
+++ precache-manifest.091f72b0cb6b589ae82949f2fc922364.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "de752fa3785da4ccd4ce",
+    "url": "/my-reads-udacity-nd/static/css/main.ce3cfc94.chunk.css"
+  },
+  {
+    "revision": "de752fa3785da4ccd4ce",
+    "url": "/my-reads-udacity-nd/static/js/main.de752fa3.chunk.js"
+  },
+  {
+    "revision": "8d2078e608697366ca74",
+    "url": "/my-reads-udacity-nd/static/js/1.8d2078e6.chunk.js"
+  },
+  {
+    "revision": "88d35423c3d5453067d0",
+    "url": "/my-reads-udacity-nd/static/js/runtime~main.88d35423.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/my-reads-udacity-nd/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/my-reads-udacity-nd/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/my-reads-udacity-nd/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "c20353a1a2482f232b4888e820c29cfb",
+    "url": "/my-reads-udacity-nd/index.html"
+  }
+];
